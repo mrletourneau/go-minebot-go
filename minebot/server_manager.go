7bb8bddedf5a24e2003b5797f38acd66e64b3b8f
Alexander Letourneau <letourneau@samsix.com>
2019-03-31 12:03:59 -0400

    Added file to wrap AWS server stuff

---
+++ minebot/server_manager.go
@@ -0,0 +1,16 @@
+package minebot
+
+type Ec2Wrapper struct{
+
+}
+
+func (ec2Wrapper *Ec2Wrapper) startServer() {
+
+}
+
+func (ec2Wrapper *Ec2Wrapper) stopServer() {
+
+}
+
+type Ec2WrapperFactory struct{}
+
